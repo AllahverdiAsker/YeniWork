ae1af24bd405a5bc05ae8809fa3522118e130731
Allahverdi Asgarli <allahverdiaa@code.edu.az>
2022-06-01 17:19:31 -0700

    yeniwork

---
+++ YeniWork/YeniWork/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace YeniWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            User user1 = new User();
+
+            user1.SetUsername("Hasan Pashadost");
+            user1.SetPassword("Whatisthis3");
+
+            Console.WriteLine(user1.GetUserName());
+            Console.WriteLine(user1.GetPassword());
+
+
+        }
+    }
+}
---
+++ YeniWork/YeniWork/User.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YeniWork
+{
+    class User
+    {
+        private string _username;
+        private string _password;
+        
+        
+            
+            public void SetUsername(string username)
+
+            {
+
+            if(username!=null && username.Length>5 && username.Length<26)
+            {
+                _username = username;
+
+            }
+
+            }
+            public void SetPassword(string password)
+            {
+            if(password.Length>7 && password.Length < 26)
+            {
+                for(int i = 0; i < password.Length; i++)
+                {
+                    
+                    if (HasDigit(password)&&HasUpper(password)&& HasLower(password))
+                    {
+
+                        _password = password;
+
+                    }
+                }
+            }            
+            }
+        public bool HasDigit(string password)
+        {
+            for(int i = 0; i < password.Length; i++)
+            {
+                if (char.IsDigit(password[i]));
+                return true;
+
+            }
+            return false;
+        }
+        public bool HasUpper(string password)
+        {
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsUpper(password[i])) ;
+                return true;
+
+            }
+            return false;
+        }
+        public bool HasLower(string password)
+        {
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsLower(password[i])) ;
+                return true;
+
+            }
+            return false;
+        }
+
+        public string GetUserName()
+        {
+            return _username;
+        }
+
+
+        public string GetPassword()
+        {
+            return _password;
+        }
+
+    }
+}
